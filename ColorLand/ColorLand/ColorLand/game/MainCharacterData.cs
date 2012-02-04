4df39e64cdb79d3fdb8b72e7bde66e4776ed1bdb
andrehbleitao <andrehbleitao@gmail.com>
2012-02-04 18:46:24 +0000

    2o commit. Torcer pra nao encaralhar.

---
+++ ColorLand/ColorLand/ColorLand/game/MainCharacterData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ColorLand
+{
+    class MainCharacterData
+    {
+
+        private int mEnergy;
+        private int mColor;
+
+        //properties
+        public int pEnergy
+        {
+            get
+            {
+                return mEnergy;
+            }
+            set
+            {
+                mEnergy = value;
+            }
+        }
+
+        public int pColor
+        {
+            get
+            {
+                return mColor;
+            }
+            set
+            {
+                mColor = value;
+            }
+        }
+
+
+
+    }
+}
